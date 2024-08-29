fdcc680f7dd540049d36d2e8ca704fff51ab8f9b
arun24hrs <2017arun.rana@gmail.com>
2024-08-29 22:18:15 +0530

    updated resume

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -162,7 +162,7 @@ export default function Nav() {
         </a>
         <Button
         id="resume-button-1"
-              onClick={()=>{window.open('https://1drv.ms/b/s!Aq8UXXTyXYSvnUnz5wdsasOzqiEy?e=iJznps',"_blank")}}
+              onClick={()=>{window.open('https://1drv.ms/b/c/af845df2745d14af/EYlh4TTt6nVPlBkuD9Pi0NIBPK3adBmw6aISG8NlBxNKJg?e=qEBFla',"_blank")}}
               variant="solid"
               colorScheme={"brand"}
               rounded={"full"}
